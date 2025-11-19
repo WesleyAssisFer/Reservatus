cd53dc93c1e53f47697b8f2e1197442dcccc1655
Wesley Assis <wesleyassis2002@gmail.com>
2025-11-19 15:59:19 -0300

    feat: AgendamentoController concluido

--- src/main/java/com/grupoBL8/Reservatus/Agendamento/Controller/AgendamentoController.java
+++ src/main/java/com/grupoBL8/Reservatus/Agendamento/Controller/AgendamentoController.java
@@ -1,4 +1,57 @@
 package com.grupoBL8.Reservatus.Agendamento.Controller;
 
+import com.grupoBL8.Reservatus.Agendamento.Model.AgendamentoModel;
+import com.grupoBL8.Reservatus.Agendamento.Service.AgendamentoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agendamento")
 public class AgendamentoController {
+
+    // Dependecia
+    private final AgendamentoService agendamentoService;
+    public AgendamentoController(AgendamentoService agendamentoService){
+        this.agendamentoService = agendamentoService;
+    }
+
+    // Listar Todos
+    @GetMapping("/listar")
+    public ResponseEntity<List<AgendamentoModel>> listarTodos(){
+        return ResponseEntity.ok(agendamentoService.listarTodos());
+    }
+
+    // Listar Por id
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<AgendamentoModel> listarPorId(@PathVariable Long id){
+        return agendamentoService.listarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Salvar
+    @PostMapping("/salvar")
+    public ResponseEntity<AgendamentoModel> salvar(@RequestBody AgendamentoModel agendamentoModel){
+        return ResponseEntity.ok(agendamentoService.salvar(agendamentoModel));
+    }
+
+    // Atualizar
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<AgendamentoModel> atualizar(@PathVariable Long id, @RequestBody AgendamentoModel agendamentoModel){
+        return agendamentoService.atualizar(id, agendamentoModel)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Deletar
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletar(@PathVariable Long id){
+        if(agendamentoService.deletar(id)){
+            return ResponseEntity.ok("Agendamento de Id " + id + " deletado com sucesso");
+        }else{
+            return ResponseEntity.status(404).body("Agendamento de Id " + id + " nao encontrado");
+        }
+    }
 }

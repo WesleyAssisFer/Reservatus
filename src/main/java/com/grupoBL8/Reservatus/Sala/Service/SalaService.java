7664c83cdf402c93258b83235315d17be4ad289a
Wesley Assis <wesleyassis2002@gmail.com>
2025-11-22 16:58:16 -0300

    refactor: SalaService com DTO

--- src/main/java/com/grupoBL8/Reservatus/Sala/Service/SalaService.java
+++ src/main/java/com/grupoBL8/Reservatus/Sala/Service/SalaService.java
@@ -2,41 +2,53 @@ package com.grupoBL8.Reservatus.Sala.Service;
 
 import com.grupoBL8.Reservatus.Sala.Model.SalaModel;
 import com.grupoBL8.Reservatus.Sala.Repository.SalaRepository;
+import com.grupoBL8.Reservatus.Sala.SalaDTO;
+import com.grupoBL8.Reservatus.Sala.SalaMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SalaService {
     // Dependencia
     private final SalaRepository salaRepository;
+    private final SalaMapper salaMapper;
 
-    public SalaService(SalaRepository salaRepository){
+    public SalaService(SalaRepository salaRepository, SalaMapper salaMapper){
         this.salaRepository = salaRepository;
+        this.salaMapper = salaMapper;
     }
 
     // Listar Todos
-    public List<SalaModel> listarTodos(){
-        return salaRepository.findAll();
+    public List<SalaDTO> listarTodos(){
+        List<SalaModel> modelListarTodos = salaRepository.findAll();
+        return modelListarTodos.stream()
+                .map(salaMapper::map)
+                .collect(Collectors.toList());
     }
 
     // Listar por Id
-    public Optional<SalaModel> listarPorId(Long id){
-        return salaRepository.findById(id);
+    public Optional<SalaDTO> listarPorId(Long id){
+        Optional<SalaModel> modelListarId = salaRepository.findById(id);
+        return modelListarId.map(salaMapper::map);
     }
 
     // Salvar
-    public SalaModel salvar(SalaModel salaModel){
-        return salaRepository.save(salaModel);
+    public SalaDTO salvar(SalaDTO salaDTO){
+        SalaModel model = salaMapper.map(salaDTO);
+       SalaModel salvarModel = salaRepository.save(model);
+        return salaMapper.map(salvarModel);
     }
 
     // Atualizar
-    public Optional<SalaModel> atualizar( Long id,SalaModel salaAtualizada){
+    public Optional<SalaDTO> atualizar( Long id,SalaDTO salaDTO){
         return salaRepository.findById(id)
                 .map(sala -> {
-                    sala.setNome(salaAtualizada.getNome());
-                    return salaRepository.save(sala);
+                    sala.setNome(salaDTO.getNome());
+                    SalaModel salvarModel = salaRepository.save(sala);
+                    return salaMapper.map(salvarModel);
                 });
     }
 
